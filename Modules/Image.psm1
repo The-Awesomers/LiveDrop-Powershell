c5a4b57393eaa859c82cf61b9271038fc6d36330
LBrown8534 <lukeman.brown@yahoo.com>
2016-11-11 22:23:07 -0500

    Update Image.psm1

--- Modules/Image.psm1
+++ Modules/Image.psm1
@@ -1,1 +1,10 @@
+Write-Host "This operation will create an image of the specified partition(s) and place the image(s) into a specified destination."
+$start = Read-Host -Prompt "Continue? (y/N)"
+If ( $start -ne "y" ) {exit}
+
+
+$sourdir = Read-Host -Prompt "What partition(s) would you like to image? [Format: X:,Y:,Z:] "
+$allcrit = Read-Host -Prompt "Are operating system files located on the specified partition(s)? (y/N) "
+$destdir = Read-Host -Prompt "Where would you like the image stored? [Format: X:]"
+
 
