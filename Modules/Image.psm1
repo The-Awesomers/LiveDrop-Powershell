427bdedb0394717c8fb6250fac03560c213e6be5
LBrown8534 <lukeman.brown@yahoo.com>
2016-11-17 17:33:39 -0500

    Update Image.psm1

--- Modules/Image.psm1
+++ Modules/Image.psm1
@@ -2,10 +2,13 @@ Write-Host "This operation will create an image of the specified partition(s) an
 $start = Read-Host -Prompt "Continue? (y/N)"
 If ( $start -ne "y" ) {exit}
 
-
+::Get-Volume right here doesnt quite work. What now? ::
 $sourdir = Read-Host -Prompt "What partition(s) would you like to image? [Format: X:,Y:,Z:] "
 $allcrit = Read-Host -Prompt "Are operating system files located on the specified partition(s)? (y/N) "
-$destdir = Read-Host -Prompt "Where would you like the image stored? [Format: X:\pathname]"
+$desttype = Read-Host -Prompt "are the file being saved to a shared folder or local volume? [ 0 = shared folder , 1 = local volume ]"
+If ( $desttype -eq "0" ) {$destdir = Read-Host -Prompt "Where would you like the image stored? [Format: \\shared_folder\pathname]"}
+If ( $desttype -eq "1" ) {$destdir = Read-Host -Prompt "Where would you like the image stored? [Format: X:(\pathname)]"}
+
 $logpath = get-date -UFormat '%a_%d_%b_%Y_%T(%Z)' | foreach {$_ -replace ":", "."}
 
 If ( $allcrit -eq "y" ) { $osfiles = "-allCritical" }
