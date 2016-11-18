1901ab07fe78e14919d0b7c46fd009dce62063a3
LBrown8534 <lukeman.brown@yahoo.com>
2016-11-17 19:03:07 -0500

    Update Image.psm1

--- Modules/Image.psm1
+++ Modules/Image.psm1
@@ -1,11 +1,14 @@
+# Lucas Brown #
+
 Write-Host "This operation will create an image of the specified partition(s) and place the image(s) into a specified destination."
 $start = Read-Host -Prompt "Continue? (y/N)"
 If ( $start -ne "y" ) {exit}
 
-::Get-Volume right here doesnt quite work. What now? ::
+Get-Volume | Out-Host
+
 $sourdir = Read-Host -Prompt "What partition(s) would you like to image? [Format: X:,Y:,Z:] "
 $allcrit = Read-Host -Prompt "Are operating system files located on the specified partition(s)? (y/N) "
-$desttype = Read-Host -Prompt "are the file being saved to a shared folder or local volume? [ 0 = shared folder , 1 = local volume ]"
+$desttype = Read-Host -Prompt "Are the file being saved to a shared folder or local volume? [ 0 = shared folder , 1 = local volume ]"
 If ( $desttype -eq "0" ) {$destdir = Read-Host -Prompt "Where would you like the image stored? [Format: \\shared_folder\pathname]"}
 If ( $desttype -eq "1" ) {$destdir = Read-Host -Prompt "Where would you like the image stored? [Format: X:(\pathname)]"}
 
@@ -15,3 +18,6 @@ If ( $allcrit -eq "y" ) { $osfiles = "-allCritical" }
 
 echo "wbAdmin start backup -backupTarget:$destdir -include:$sourdir $osfiles | Out-File $destdir\WindowsImageBackup\$logpath.txt
 "
+
+
+# Lucas Brown #
